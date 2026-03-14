c6cf2d21237b58f2ab7064d890f90fa0054066ea
spicyshibe <r.rasydan.w@gmail.com>
2026-03-14 22:00:29 +0700

    initial commit

---
+++ src/main/java/com/deploy/PraktikumKTP/PraktikumKtpApplication.java
@@ -0,0 +1,13 @@
+package com.deploy.PraktikumKTP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraktikumKtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraktikumKtpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deploy/PraktikumKTP/PraktikumKtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploy.PraktikumKTP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraktikumKtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
