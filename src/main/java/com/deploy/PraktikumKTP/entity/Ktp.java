cf481d1c26b68ea4967662b152277a61949a25f3
spicyshibe <r.rasydan.w@gmail.com>
2026-03-14 22:38:31 +0700

    Entity file

---
+++ src/main/java/com/deploy/PraktikumKTP/entity/Ktp.java
@@ -0,0 +1,21 @@
+package com.deploy.PraktikumKTP.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "ktp")
+@Data
+public class Ktp {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String nomorKtp;
+    private String namaLengkap;
+    private String alamat;
+    private LocalDate tanggalLahir;
+    private String jenisKelamin;
+}
